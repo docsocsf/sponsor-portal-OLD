f12b1ef35d7a5b44274de653f9a533d3a207c9f2
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2017-09-21 01:45:40 +0100

    List all on sponsor page

--- client/src/views/Sponsors/SponsorProfile.jsx
+++ client/src/views/Sponsors/SponsorProfile.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
 export default class SponsorProfile extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cvs: []
+    }
+  }
+
+  componentDidMount = async () => {
+    this.getCVs()
+  }
+
+  getCVs = async () => {
+    try {
+      const cvs = await this.props.fetchCVs()
+      if (!cvs) return
+      this.setState({cvs})
+    } catch (e) {
+      console.log("fetch cvs", e)
+    }
+  }
+
   render() {
+    let {cvs} = this.state;
+    cvs = cvs.map((cv, i) => <li key={i}>{cv.name}</li>)
     return (
-      <h1>Hello, Sponsor</h1>
+      <div>
+        <h1>Hello, Sponsor</h1>
+        <ul>
+          {cvs}
+        </ul>
+      </div>
     );
   }
 }
--- client/src/views/Sponsors/index.js
+++ client/src/views/Sponsors/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import fetchWithConfig from '../../fetch';
+import getJWTHeader from '../../jwt';
 import SponsorProfile from './SponsorProfile';
+import '../../../style/sponsors.scss';
 
-export default SponsorProfile;
+const fetchCVs = async () => {
+  const headers = await getJWTHeader();
+  return await fetchWithConfig('/sponsors/api/cvs', { headers, json: true })
+}
+
+export default () => <SponsorProfile fetchCVs={fetchCVs} />;
