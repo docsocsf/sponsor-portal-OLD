1ebdeee98e9b4e1989cb6f7a2e7fb2dd280ea586
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2017-10-22 00:50:48 +0100

    Restyle login form (#27)
    * Restyle login form
    
    * Include images in the gitignore

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from 'Components/Header';
 import md5 from 'md5';
 import request from 'superagent';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -15,19 +16,14 @@ export default class Login extends React.Component {
 
   onSubmit = async (event) => {
     event.preventDefault()
-    let { email, password } = this.state;
-    password = md5(password)
     try {
       let resp = await request
         .post('/auth/sponsors/login')
-        .type('form')
-        .send({ email })
-        .send({ password })
+        .send(new FormData(document.getElementById("sponsor-login-form")))
       let redirect = new URL(resp.xhr.responseURL).pathname;
       window.location.replace(redirect)
     } catch (e) {
-      console.log(e);
-      this.setState({error: "The email and/or password you entered was incorrect"});
+      this.setState({error: "The email and/or password you entered was incorrect."});
     }
   }
 
@@ -37,29 +33,57 @@ export default class Login extends React.Component {
     let {error, email, password } = this.state;
 
     return (
-      <div>
-        <Header />
-        <div id="login-form">
-          { !!error &&
-              <h4 className="error">{error}</h4>
-          }
-          <form onSubmit={this.onSubmit}>
-            <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={this.handleChange}/>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={this.handleChange}/>
-            <input type="submit" value="Login" />
-          </form>
+      <div id="login-view">
+        <div id="login">
+          <img className="logo" src="/assets/images/docsoc-sf-logo.svg" width="60"/>
+          <h1>
+            DoCSoc<br/>Sponsor Portal
+          </h1>
+          <Tabs>
+            <TabList>
+              <Tab>Student Login</Tab>
+              <Tab>Sponsor Login</Tab>
+            </TabList>
+
+            <TabPanel>
+                <button>
+                  <a href="/students">Login with Imperial</a>
+                </button>
+            </TabPanel>
+            <TabPanel>
+              <LoginForm
+                onSubmit={this.onSubmit}
+                handleChange={this.handleChange}
+                id="sponsor-login-form"
+                email={email}
+                password={password}
+                error={error}
+              />
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
     );
   }
 }
+
+const LoginForm = (props) => (
+  <form onSubmit={props.onSubmit} id={props.id} className="stacked centered">
+    { !!props.error &&
+        <p className="error">{props.error}</p>
+    }
+    <input
+      type="text"
+      name="email"
+      placeholder="Email"
+      value={props.email}
+      onChange={props.handleChange}/>
+    <input
+      type="password"
+      name="password"
+      placeholder="Password"
+      value={props.password}
+      onChange={props.handleChange}/>
+    <input type="submit" value="Login" />
+  </form>
+);
